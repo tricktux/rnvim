3e580c9f5a41b523019c0801d984fed865ab278c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-04-16 08:39:12 -0400

    Implemented getter functions

--- src/cli_args.cpp
+++ src/cli_args.cpp
@@ -29,12 +29,36 @@ void CliArgs::init(ICliArgsGetter &getter) {
 		arg.second = getter.get_arg(arg.first.c_str(), arg.second);
 }
 
-int get_arg(const char *name, int def) {
-	return 8;
+int CliArgs::get_arg(const char *name, int def) {
+	if ((name == nullptr) || (name[0] == 0))
+		return def;
+
+	const auto &search = int_args.find(name);
+
+	if (search == int_args.end())
+		return def;
+
+	return search->second;
 }
-std::string get_arg(const char *name, const std::string &def) {
-	return "lkas";
+std::string CliArgs::get_arg(const char *name, const std::string &def) {
+	if ((name == nullptr) || (name[0] == 0))
+		return def;
+
+	const auto &search = str_args.find(name);
+
+	if (search == str_args.end())
+		return def;
+
+	return search->second;
 }
-bool get_arg(const char *name, bool def) {
-	return false;
+bool CliArgs::get_arg(const char *name, bool def) {
+	if ((name == nullptr) || (name[0] == 0))
+		return def;
+
+	const auto &search = bool_args.find(name);
+
+	if (search == bool_args.end())
+		return def;
+
+	return search->second;
 }
