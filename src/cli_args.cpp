86c2f3317b13c1c4df164909953eafd2a0969e43
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-04-22 02:22:52 -0400

    Finished implementation of mingled parse_and_save_options

--- src/cli_args.cpp
+++ src/cli_args.cpp
@@ -19,31 +19,55 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string>
-#include <vector>
 #include <utility>
+#include <vector>
 
 #include "cli_args.hpp"
 #include "common.hpp"
 #include "cxxopts.hpp"
 #include "easylogging/easylogging++.h"
 
-cxxopts::ParseResult&& cli::CliArgs::parse_options(int argc, char **argv) {
+int cli::CliArgs::parse_and_save_options(int argc, char **argv,
+                                         cli::Options &opt_save) {
   try {
 
-		// if (argc < 2) {
-			// DLOG(INFO) << "[" << __FUNCTION__ << "]: No arguments. Yay!!";
-			// return SUCCESS;
-		// }
+    if (argc < 2) {
+      DLOG(INFO) << "[" << __FUNCTION__ << "]: No arguments. Yay!!";
+      return SUCCESS;
+    }
 
-		opt.parse_positional(pos_arg.data());
-		return std::move(opt.parse(argc, argv));
+    opt.parse_positional(pos_arg.data());
+    auto res = opt.parse(argc, argv);
 
+		const char *p;
+		for (auto &arg : opt_save.str_args) {
+			p = arg.first.data();
+			if (res.count(p)) {
+				arg.second.first = res[p].as<std::string>();
+			}
+		}
+		for (auto &arg : opt_save.int_args) {
+			p = arg.first.data();
+			if (res.count(p)) {
+				arg.second.first = res[p].as<int>();
+			}
+		}
+		for (auto &arg : opt_save.bool_args) {
+			p = arg.first.data();
+			if (res.count(p)) {
+				arg.second.first = res[p].as<bool>();
+			}
+		}
+		p = std::get<0>(opt_save.pos_arg).data();
+		if (res.count(p)) {
+			opt_save.set_pos_arg(res[p].as<std::vector<std::string>>());
+		}
 
-		// return SUCCESS;
+    return SUCCESS;
   } catch (const cxxopts::OptionException &excep) {
     DLOG(ERROR) << "[" << __FUNCTION__
                 << "]: Error parsing options: " << excep.what();
-    // return -100;
+    return -100;
   }
 }
 
@@ -155,32 +179,33 @@ bool cli::Options::get_arg(std::string_view name, bool def) const {
 }
 
 void cli::Options::set_arg(std::string_view name, int val) {
-	if (name.empty())
-		return;
+  if (name.empty())
+    return;
 
-	auto search = int_args.find(name.data());
-	if (search == int_args.end())
-		return;
+  auto search = int_args.find(name.data());
+  if (search == int_args.end())
+    return;
 
-	search->second.first = val;
+  search->second.first = val;
 }
 void cli::Options::set_arg(std::string_view name, std::string_view val) {
-	if (name.empty())
-		return;
+  if (name.empty())
+    return;
 
-	auto search = str_args.find(name.data());
-	if (search == str_args.end())
-		return;
+  auto search = str_args.find(name.data());
+  if (search == str_args.end())
+    return;
 
-	search->second.first = val;
+  search->second.first = val;
 }
+
 void cli::Options::set_arg(std::string_view name, bool val) {
-	if (name.empty())
-		return;
+  if (name.empty())
+    return;
 
-	auto search = bool_args.find(name.data());
-	if (search == bool_args.end())
-		return;
+  auto search = bool_args.find(name.data());
+  if (search == bool_args.end())
+    return;
 
-	search->second.first = val;
+  search->second.first = val;
 }
