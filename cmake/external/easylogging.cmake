fa18b5a9f200bd8e9d8d77ba67a16ed6d9afe8a2
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-10-19 17:06:13 -0400

    Renamed cmake/download to cmake/external
    Also added alpha version for easylogging as an external project

---
+++ cmake/external/easylogging.cmake
@@ -0,0 +1,30 @@
+
+include(ExternalProject)
+
+# build directory
+set(easylogging_PREFIX ${CMAKE_BINARY_DIR}/external/easylogging-prefix)
+# install directory
+set(easylogging_INSTALL ${CMAKE_BINARY_DIR}/external/easylogging-install)
+
+ExternalProject_Add(
+	easylogging
+	GIT_REPOSITORY https://github.com/zuhd-org/easyloggingpp
+	GIT_TAG				 v9.96.7
+	GIT_SHALLOW		 1
+	GIT_PROGRESS	 1
+	INSTALL_DIR ${easylogging_INSTALL}
+	CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+						-DCMAKE_INSTALL_PREFIX=${easylogging_INSTALL}
+						-DBUILD_STATIC_LIBS=ON
+						-DBUILD_TEST=OFF
+
+	LOG_DOWNLOAD 1
+	LOG_INSTALL 1
+)
+
+set(EASYLOGGING_FOUND TRUE)
+set(EASYLOGGING_INCLUDE_DIRS ${easylogging_INSTALL}/include)
+set(EASYLOGGING_LIBRARIES ${easylogging_INSTALL}/lib/libeasyloggingpp.a
+	${CMAKE_THREAD_LIBS_INIT})
+set(EASYLOGGING_LIBRARY_DIRS ${easylogging_INSTALL}/lib)
+set(EASYLOGGING_EXTERNAL TRUE)
