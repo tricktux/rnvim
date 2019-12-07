fd9803f59f569cbc1ab2b345f9c1cc18b00293aa
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-12-07 12:57:12 -0500

    Fixing reproc

--- cmake/external/reproc++.cmake
+++ cmake/external/reproc++.cmake
@@ -13,13 +13,14 @@ ExternalProject_Add(
 	reproc
 	PREFIX ${libnvc_PREFIX}
 	GIT_REPOSITORY https://github.com/DaanDeMeyer/reproc
-	GIT_TAG				 v9.0.0
+	# Using latest to match example code
+	# GIT_TAG				 v9.0.0
 	GIT_SHALLOW		 TRUE
 	GIT_PROGRESS	 TRUE
 	CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 						-DCMAKE_INSTALL_PREFIX=${reproc_INSTALL}
 						-DREPROC++=ON
-						-DREPROC_TEST=OFF
+						-DREPROC_TEST=ON
 						-DREPROC_EXAMPLES=OFF
 
 	BUILD_BYPRODUCTS ${reproc_LIBS}
