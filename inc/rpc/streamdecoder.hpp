d4d399ec57a5eddb9e77d5e6f398c524f93e26b2
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-12-19 10:12:16 -0500

    Alpha of streamcoder

---
+++ inc/rpc/streamdecoder.hpp
@@ -0,0 +1,76 @@
+/// @file streamdecoder.hpp
+/// @brief Class in charge of parsing stream output
+/// @author Reinaldo Molina
+/// @version  0.0
+/// @date Dec 19 2019
+
+// Copyright © 2019 Reinaldo Molina
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "easylogging++.h"
+#include "mpack.h"
+#include "rpc/iodevice.hpp"
+#include <optional>
+
+namespace nvimrpc {
+
+/** @brief Wrap around `MPack` Node and Stream capabilites
+ * See
+ * [here](https://github.com/ludocode/mpack/blob/develop/docs/node.mdhttps://github.com/ludocode/mpack/blob/develop/docs/node.md)
+ * */
+class StreamDecoder {
+  const int MAX_SIZE = 1048576; // 1024*1024
+  const int MAX_NODES = 1024;
+  mpack_tree_t tree; /// Gets parsed into a node
+
+  /**
+   * @brief Function used by mpack to read bytes from `stdout`
+   * This function is passed to `mpack_tree_init_stream`, so that every time
+   * `mpack_tree_try_parse` is called, this function is the one that provides
+   * the bytes.  According to `MPack` documentation in order to use
+   * `mpack_tree_try_parse` this function needs to be `async`
+   * @param ptree
+   * @param buf
+   * @param count
+   * @return
+   */
+  static size_t read_iodev(mpack_tree_t *ptree, char *buf, size_t count) {
+    if (ptree == nullptr) {
+      DLOG(ERROR) << "Invalid tree pointer";
+      return 0;
+    }
+    if (buf == nullptr) {
+      DLOG(ERROR) << "Invalid buf pointer";
+      return 0;
+    }
+    if (count == 0) {
+      DLOG(ERROR) << "Zero count provided";
+      return 0;
+    }
+
+    auto piodev = (IoDevice *)(mpack_tree_context(ptree));
+    return piodev->recv(buf, count);
+  }
+
+public:
+  StreamDecoder(IoDevice &dev) : tree() {
+    mpack_tree_init_stream(&tree, &read_iodev, &dev, MAX_SIZE, MAX_NODES);
+  }
+  ~StreamDecoder() { mpack_tree_destroy(&tree); }
+
+  std::optional<mpack_node_t> poll();
+};
+
+} // namespace nvimrpc
---
+++ src/rpc/streamdecoder.cpp
@@ -0,0 +1,48 @@
+/// @file streamdecoder.cpp
+/// @brief Class in charge of parsing stream output
+/// @author Reinaldo Molina
+/// @version  0.0
+/// @date Dec 19 2019
+// Copyright © 2019 Reinaldo Molina
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "rpc/streamdecoder.hpp"
+
+/** 
+ * @brief Parse data from the `stdout` into an `mpack_node_t`
+ * This function uses `mpack_tree_try_parse` which in turn calls `read_iodev`, 
+ * which in turn calls `iodevice->recv`, which then asynchronously retrieves 
+ * data in `output` string.
+ * `mpack_tree_root` converts the `mpack_tree_t` into a `mpack_node_t`
+ * See @ref ReprocDevice::recv
+ * @return `mpack_node_t`
+ */
+std::optional<mpack_node_t> nvimrpc::StreamDecoder::poll() {
+  if (mpack_tree_try_parse(&tree)) {
+    return mpack_tree_root(&tree);
+  }
+
+  // no message valid, two possible reasons:
+  //   1. error occurred
+  //   2. no sufficent data received
+	mpack_error_t ec = mpack_tree_error(&tree);
+	if (ec == mpack_ok) {
+		DLOG(WARNING) << "Did not received expected count of data";
+		return {};
+	}
+
+	DLOG(ERROR) << "Error: '" << mpack_error_to_string(ec) << "'";
+	return {};
+}
