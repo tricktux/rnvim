d5bc91b56fbab1088f19e68f696fdffcc97898c7
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-12-18 19:42:21 -0500

    Successful build
    I can link again with reproc

--- cmake/external/reproc++.cmake
+++ cmake/external/reproc++.cmake
@@ -7,6 +7,7 @@ set(reproc_INSTALL ${CMAKE_BINARY_DIR}/external/reproc-install)
 
 set(reproc_LIBS
 	${reproc_INSTALL}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}reproc++${CMAKE_STATIC_LIBRARY_SUFFIX}
+	${reproc_INSTALL}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}reproc${CMAKE_STATIC_LIBRARY_SUFFIX}
 	)
 
 ExternalProject_Add(
@@ -20,7 +21,7 @@ ExternalProject_Add(
 	CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 						-DCMAKE_INSTALL_PREFIX=${reproc_INSTALL}
 						-DREPROC++=ON
-						-DREPROC_TEST=ON
+						-DREPROC_TEST=OFF
 						-DREPROC_EXAMPLES=OFF
 
 	BUILD_BYPRODUCTS ${reproc_LIBS}
