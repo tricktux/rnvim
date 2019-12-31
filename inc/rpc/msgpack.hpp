4bb55a33072e1dc44395d1d8f812e351597a80dc
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-12-30 22:10:35 -0500

    Not even alpha of response
    Plus moved some long functions over to source file

--- inc/rpc/msgpack.hpp
+++ inc/rpc/msgpack.hpp
@@ -70,7 +70,8 @@ public:
 
 class IMpackReqPack {
 protected:
-	const size_t NUM_ELEMENTS = 4;
+  const size_t NUM_ELEMENTS = 4;
+  const size_t MSG_TYPE = 0;
 
 public:
   IMpackReqPack() = default;
@@ -78,7 +79,7 @@ public:
 
   virtual void set_msgid(size_t) = 0;
   virtual void set_method(std::string_view) = 0;
-	// Cannot be virtual since it uses templates
+  // Cannot be virtual since it uses templates
   // template<class...Params> void set_params(Params&&... params);
   virtual std::string build() = 0;
 };
@@ -122,27 +123,7 @@ class MPackReqPack : public IMpackReqPack {
     }
     mpack_finish_map(writer);
   }
-  void mpack_write(mpack_writer_t *writer, const object &obj) {
-    if (std::holds_alternative<bool>(obj))
-      return mpack_write(writer, std::get<bool>(obj));
-    if (std::holds_alternative<int64_t>(obj))
-      return mpack_write(writer, std::get<int64_t>(obj));
-    if (std::holds_alternative<double>(obj))
-      return mpack_write(writer, std::get<double>(obj));
-    if (std::holds_alternative<std::string>(obj))
-      return mpack_write(writer, std::get<std::string>(obj));
-    if (std::holds_alternative<std::unordered_map<std::string, object_wrapper>>(
-            obj)) {
-      return mpack_write(
-          writer,
-          std::get<std::unordered_map<std::string, object_wrapper>>(obj));
-    }
-    if (std::holds_alternative<std::vector<object_wrapper>>(obj)) {
-      return mpack_write(writer, std::get<std::vector<object_wrapper>>(obj));
-    }
-
-    DLOG(ERROR) << "Unrecognized object type!";
-  }
+  void mpack_write(mpack_writer_t *writer, const object &obj);
 
   template <typename T, typename... Params>
   void mpack_write(mpack_writer_t *writer, T value, Params &&... params) {
@@ -156,7 +137,7 @@ public:
     // The request message is a four elements array
     mpack_start_array(&writer, NUM_ELEMENTS);
     // The message type, must be the integer zero (0) for "Request" messages.
-    mpack_write_i32(&writer, 0);
+    mpack_write_i32(&writer, MSG_TYPE);
   }
   virtual ~MPackReqPack() {
     if (data != nullptr) {
@@ -194,76 +175,37 @@ public:
     mpack_write(&writer, std::forward<Params>(params)...);
     mpack_finish_array(&writer);
   }
-  /**
-   * @brief Finalize the package and return it in binary form
-   *
-   * @return The data in the string return is packaged and ready to be
-   * transmitted.
-   */
-  std::string build() override {
-    mpack_finish_array(&writer);
-    if (mpack_error_t error = mpack_writer_destroy(&writer);
-        error != mpack_ok) {
-      DLOG(ERROR) << "Error flushing and closing the underlying stream";
-      return {};
-    }
-
-    if (data == nullptr) {
-      DLOG(ERROR) << "Error invalid pointer to data";
-      return {};
-    }
-
-    if (size == 0) {
-      DLOG(ERROR) << "Error data size is zero";
-      return {};
-    }
-
-    return {data, size};
-  }
+  std::string build() override;
 };
 
 class IMpackResUnPack {
 protected:
-	const size_t NUM_ELEMENTS = 4;
-public:
-	IMpackResUnPack() = default;
-	virtual ~IMpackResUnPack() = default;
+  const size_t NUM_ELEMENTS = 4;
+  const size_t MSG_TYPE = 1;
 
-	virtual int set_data(std::string_view) = 0;
-	virtual int get_msg_type() = 0;
-	virtual int get_msgid() = 0;
-	virtual int get_error() = 0;
-	// This function cannot be virtual because it uses templates
-	// auto get_result();
+public:
+  IMpackResUnPack() = default;
+  virtual ~IMpackResUnPack() = default;
+
+  virtual int set_data(std::string_view) = 0;
+  virtual int get_msg_type() = 0;
+  virtual size_t get_msgid() = 0;
+  virtual int get_error() = 0;
+  // This function cannot be virtual because it uses templates
+  // auto get_result();
 };
 
 class MpackResUnPack : public IMpackResUnPack {
-	mpack_reader_t reader;
+  mpack_reader_t reader;
 
 public:
-	MpackResUnPack() = default;
-	virtual ~MpackResUnPack() {}
+  MpackResUnPack() = default;
+  virtual ~MpackResUnPack() = default;
 
-	/** 
-	 * @brief Initializes the stream reader with @p data
-	 * @param data Data to unpack as a response message
-	 * @return 0 in case of success, less than zero if error
-	 */
-	int set_data(std::string_view data) override {
-		if (data.empty()) {
-			DLOG(ERROR) << "Empty data provided";
-			return -1;
-		}
-
-		mpack_reader_init_data(&reader, data.data(), data.size());
-
-		if (mpack_expect_array_max(&reader, NUM_ELEMENTS) != NUM_ELEMENTS) {
-			DLOG(ERROR) << "Expected array response of size 4";
-			return -2;
-		}
-
-		return 0;
-	}
+  int set_data(std::string_view data) override;
+  int get_msg_type() override { return mpack_expect_i32(&reader); }
+  size_t get_msgid() override { return mpack_expect_u32(&reader); }
+  int get_error() override;
 };
 
 } // namespace nvimrpc
--- src/rpc/msgpack.cpp
+++ src/rpc/msgpack.cpp
@@ -18,5 +18,97 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include "rpc/msgpack.hpp"
+
+void nvimrpc::MPackReqPack::mpack_write(mpack_writer_t *writer,
+                                        const object &obj) {
+  if (std::holds_alternative<bool>(obj))
+    return mpack_write(writer, std::get<bool>(obj));
+  if (std::holds_alternative<int64_t>(obj))
+    return mpack_write(writer, std::get<int64_t>(obj));
+  if (std::holds_alternative<double>(obj))
+    return mpack_write(writer, std::get<double>(obj));
+  if (std::holds_alternative<std::string>(obj))
+    return mpack_write(writer, std::get<std::string>(obj));
+  if (std::holds_alternative<std::unordered_map<std::string, object_wrapper>>(
+          obj)) {
+    return mpack_write(
+        writer, std::get<std::unordered_map<std::string, object_wrapper>>(obj));
+  }
+  if (std::holds_alternative<std::vector<object_wrapper>>(obj)) {
+    return mpack_write(writer, std::get<std::vector<object_wrapper>>(obj));
+  }
+
+  DLOG(ERROR) << "Unrecognized object type!";
+}
+
+/**
+ * @brief Finalize the package and return it in binary form
+ *
+ * @return The data in the string return is packaged and ready to be
+ * transmitted.
+ */
+std::string nvimrpc::MPackReqPack::build() {
+  mpack_finish_array(&writer);
+  if (mpack_error_t error = mpack_writer_destroy(&writer); error != mpack_ok) {
+    DLOG(ERROR) << "Error flushing and closing the underlying stream";
+    return {};
+  }
+
+  if (data == nullptr) {
+    DLOG(ERROR) << "Error invalid pointer to data";
+    return {};
+  }
+
+  if (size == 0) {
+    DLOG(ERROR) << "Error data size is zero";
+    return {};
+  }
+
+  return {data, size};
+}
+
+/**
+ * @brief Initializes the stream reader with @p data
+ * @param data Data to unpack as a response message
+ * @return 0 in case of success, less than zero if error
+ */
+int nvimrpc::MpackResUnPack::set_data(std::string_view data) {
+  if (data.empty()) {
+    DLOG(ERROR) << "Empty data provided";
+    return -1;
+  }
+
+  mpack_reader_init_data(&reader, data.data(), data.size());
+
+  if (mpack_expect_array_max(&reader, NUM_ELEMENTS) != NUM_ELEMENTS) {
+    DLOG(ERROR) << "Expected array response of size 4";
+    return -2;
+  }
+
+  return 0;
+}
+
+int nvimrpc::MpackResUnPack::get_error() {
+  const mpack_tag_t error = mpack_peek_tag(&reader);
+  if (error.type == mpack_type_nil) {
+    mpack_discard(&reader); // Disregard the error element
+    return 0;
+  }
+
+  if (error.type == mpack_type_str) {
+    char buf[1024];
+    mpack_expect_cstr(&reader, buf, sizeof(buf));
+    DLOG(ERROR) << "Response message contains error message: '" << buf << "'";
+    return -1;
+  }
+  if (error.type == mpack_type_int) {
+    int rc = mpack_expect_int(&reader);
+    DLOG(ERROR) << "Response message contains error code: '" << rc << "'";
+    return rc;
+  }
+
+  DLOG(ERROR) << "Response message contains error of unknown type";
+  mpack_discard(&reader); // Disregard the error element
+  return -1;
+}
