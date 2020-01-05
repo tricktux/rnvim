46483c74c386a4147c48ca98c341dc8e66b9b45a
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-01-04 22:26:02 -0500

    Adding new mpack tests

---
+++ tests/test_mpack.cpp
@@ -0,0 +1,16 @@
+/// @file test_mpack.cpp
+/// @brief Test reading and writing mpack blobs
+/// @author Reinaldo Molina
+/// @version  0.0
+/// @date Jan 04 2020
+
+
+#include "easylogging++.h"
+#include "msgpack.hpp"
+
+#include <gtest/gtest.h>
+
+INITIALIZE_EASYLOGGINGPP
+
+TEST(mpack, simple) {
+}
